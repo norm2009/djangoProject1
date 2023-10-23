89fd523791bc37a85f5db27895150cbff0c92290
Nikita <norm2009@ya.ru>
2023-10-24 00:44:21 +0300

    Add Place model

--- places_app/models.py
+++ places_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
-# Create your models here.
-print("hello!")
+class Place(models.Model):
+
+  def __str__(self):
+    return '%s' % (self.title)
+
+  title = models.CharField(max_length=200)
+  text = models.JSONField(max_length=1000, default='')
+
+# Register your models here.
