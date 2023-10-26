1c33a2c2f17739bf7fb8583e92637e864a283a34
Nikita <norm2009@ya.ru>
2023-10-26 22:11:41 +0300

    Add model for images

--- places_app/models.py
+++ places_app/models.py
@@ -1,6 +1,18 @@
+import uuid
+
 from django.db import models
 #from django.contrib.gis.db import models
 
+class Images(models.Model):
+  def __str__(self):
+    return '%s %s' % (self.indifer, self.place)
+  LIST_NUMBER = (['1', '1'], ['2', '2'])
+  indifer = models.CharField(choices=LIST_NUMBER, max_length=1)
+  place = models.ForeignKey('Place', on_delete=models.SET_NULL, null=True)
+  image = models.ImageField(unique=True)
+
+  class Meta:
+    unique_together = ('indifer', 'place')
 class Place(models.Model):
 
   def __str__(self):
